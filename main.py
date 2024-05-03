a6fb783e423f43a5da9d15e9a1b69b86b0e80c0b
Daniil0309 <168443708+Daniil0309@users.noreply.github.com>
2024-05-03 18:03:49 +0300

    img add

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("m.jpg")
+icon = pygame.image.load("img/m.jpg")
 pygame.display.set_icon(icon)
 
-target_img = pygame.image.load("target.png")
+target_img = pygame.image.load("img/target.png")
 target_width = 100
 target_height = 100
 
