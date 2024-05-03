64e45287595856d7de7f44049eca28b35030726c
Daniil0309 <168443708+Daniil0309@users.noreply.github.com>
2024-05-03 16:00:12 +0300

    add const1

--- main.py
+++ main.py
@@ -2,6 +2,18 @@ import pygame
 
 pygame.init()
 
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+pygame.display.set_caption("Игра Тир")
+icon = pygame.image.load("msg6723183026-36889.jpg")
+pygame.display.set_icon(icon)
+
+target_img = pygame.image.load("")
+target_width = 50
+target_height = 50
+
 running = True
 while running:
     pass
