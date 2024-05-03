58946ffda84e17ec4eed3c3450044cf48c483d6d
Daniil0309 <168443708+Daniil0309@users.noreply.github.com>
2024-05-03 17:52:17 +0300

    add new m jpg

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Игра Тир")
-icon = pygame.image.load("msg.jpg")
+icon = pygame.image.load("m.jpg")
 pygame.display.set_icon(icon)
 
 target_img = pygame.image.load("target.png")
